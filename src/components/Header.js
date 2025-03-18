a9350b45e35237bb049c47c39c9c7364fb42d7e4
sebabainottiiag <sebastian.bainotti@iaggbs.com>
2025-03-18 08:54:45 +0100

    changes

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+        <div class="logo">
+            <img src="images/logo2.png" alt="Logo" style={{marginRight: "0.5rem", width:"100px"}} />
+        </div>
+        <div class="user-info">
+            <a href='https://prendia.shop'><img src="images/user.png" alt="User Avatar" style={{}} /></a>
+        </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 const Home = () => {
   const [searchQuery, setSearchQuery] = useState('');
@@ -8,6 +9,7 @@ const Home = () => {
   const [promptData, setPromptData] = useState({ event: "", style: "", budget: "" });  
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [animateUp, setAnimateUp] = useState(false);
 
   const handleSearch = async () => {
     if (!searchQuery.trim()) return;
@@ -26,12 +28,16 @@ const Home = () => {
       if (!response.ok) {
         throw new Error('Error al buscar productos');
       }
-
       const data = await response.json();
-      setProducts(data); // Guardamos los productos en el estado
+      setProducts(data);
+      setTimeout(() => {
+        setAnimateUp(true);
+      }, 1000);
+
+      
     } catch (error) {
       console.error('Error:', error);
-      setProducts([]); // Vacía la lista en caso de error
+      setProducts([]); 
     } finally {
         setIsLoading(false); 
     }
@@ -65,111 +71,74 @@ const Home = () => {
   return (
     <>
       {/* HEADER */}
-      <div className="banner_bg_main">
-      <div className="logo_section">
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-12">
-              <div className="logo"><a href="index.html"><img src="images/logo.png" alt="Logo" /></a></div>
+      <Header />
+      <main className="main-container">
+        <div className={`input-container ${animateUp ? 'move-up' : ''}`}>
+          <button className="btn-ideas" onClick={() => setIsPopupOpen(!isPopupOpen)}>
+            <img src="https://img.icons8.com/ios-filled/50/000000/idea.png" alt="Idea Icon" />
+            Dame ideas
+          </button>
+          <textarea type="text" placeholder="Qué tienes en mente?" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+          <button className="btn-enviar" onClick={handleSearch} disabled={isLoading}>
+            {isLoading ? "⏳" : "Enviar"}
+          </button>
+          {isPopupOpen && (
+          <div className="idea-popup-overlay">
+            <div className="idea-popup-close" onClick={() => setIsPopupOpen(false)}>✖</div>
+            <div className="idea-popup-option" onClick={() => { 
+              setSearchQuery("Remeras blancas"); 
+              setIsPopupOpen(false); 
+            }}>
+              Remeras blancas
+            </div>
+            <div className="idea-popup-option" onClick={() => { 
+              setSearchQuery("Pantalones grises de boda"); 
+              setIsPopupOpen(false); 
+            }}>
+              Pantalones grises de boda
+            </div>
+            <div className="idea-popup-option" onClick={() => { 
+              setSearchQuery("Camisas formales azules"); 
+              setIsPopupOpen(false); 
+            }}>
+              Camisas formales azules
+            </div>
+            <div className="idea-popup-option" onClick={() => { 
+              setSearchQuery("Zapatos elegantes negros"); 
+              setIsPopupOpen(false); 
+            }}>
+              Zapatos elegantes negros
             </div>
           </div>
+        )}
         </div>
-      </div>
-
-      <div className="header_section">
-        <div className="container">
-          <div className="containt_main">
-            <div className="main">
-              <div className="input-group chatgpt-container">
-                <textarea
-                  className="chatgpt-input"
-                  placeholder="¿Qué tienes en mente?"
-                  value={searchQuery}
-                  onChange={(e) => setSearchQuery(e.target.value)}
-                ></textarea>
-                <div className="buttons-container">
-                  <button className="chatgpt-send-button" onClick={handleSearch} disabled={isLoading}>
-                  {isLoading ? "⏳" : "➤"}
-                  </button>
+        {isLoading && (
+          <div className="loading-message">
+            🔍 Buscando productos... Por favor espera.
+          </div>
+        )}
+        {products.length > 0 && (
+          <div className="products-container">
+            {products.map((product, index) => (
+              <div key={index} className="product-card">
+                <h6>{product.name}</h6>
+                <p><strong>Precio:</strong> €{product.price}</p>
+                <div className='card-image'>
+                  <a href={product.url} target="_blank" rel="noopener noreferrer">
+                    <img src={`data:image/png;base64,${product.image}`} alt={product.name} />
+                  </a>
                 </div>
+                <p className='description'>{product.description}</p>
+                <p><strong>Talles:</strong> {product.sizes.join(', ')}</p>
               </div>
-              <div className="help-link-container">
-                <a href="#" className="help-link" onClick={(e) => { e.preventDefault(); openPopup(); }}>
-                  Dame una idea
-                </a>
-              </div>
-            </div>
+            ))}
           </div>
-        </div>
-      </div>
-      {isLoading && (
-        <div className="loading-message">
-          🔍 Buscando productos... Por favor espera.
-        </div>
-      )}
-      {/* Popup Modal */}
-      {isPopupOpen && (
-        <div className="popup-overlay">
-          <div className="popup-content">
-            <button className="popup-close" onClick={closePopup}>✖</button>
-            {helpStep === 1 && (
-              <>
-                <p>¿Para qué evento necesitas la ropa?</p>
-                <button onClick={() => nextHelpStep("una boda")}>Una boda</button>
-                <button onClick={() => nextHelpStep("una fiesta")}>Una fiesta</button>
-                <button onClick={() => nextHelpStep("un evento formal")}>Evento formal</button>
-              </>
-            )}
-            {helpStep === 2 && (
-              <>
-                <p>¿Qué estilo prefieres?</p>
-                <button onClick={() => nextHelpStep("elegante")}>Elegante</button>
-                <button onClick={() => nextHelpStep("casual")}>Casual</button>
-                <button onClick={() => nextHelpStep("moderno")}>Moderno</button>
-              </>
-            )}
-            {helpStep === 3 && (
-              <>
-                <p>¿Cuál es tu presupuesto?</p>
-                <button onClick={() => nextHelpStep("100")}>100€</button>
-                <button onClick={() => nextHelpStep("500")}>500€</button>
-                <button onClick={() => nextHelpStep("1000")}>1000€</button>
-              </>
-            )}
-          </div>
-        </div>
-      )}
-    </div>
+        )}
+
+        
+      </main>
 
 
-      {products.length > 0 && (
-        <div className="fashion_section content-container">
-          <div className="container">
-            <div className="row">
-              {products.map((product, index) => (
-                <div key={index} className="col-lg-4 col-sm-4 p-6">
-                  <div className="box_main">
-                    <h4 className="product_text">{product.name}</h4>
-                    <p className="price_text">
-                      Price <span style={{ color: "#262626" }}>${product.price}</span>
-                    </p>
-                    <div className="product_img">
-                      <img src={`data:image/png;base64,${product.image}`} alt={product.name} />
-                    </div>
-                    <p className='product_desc'>{product.description}</p>
-                    <p><strong>Sizes:</strong> {product.sizes.join(', ')}</p>
-                    <div className="btn_main">
-                      <div className="buy_bt">
-                        <a href={product.url} target="_blank" rel="noopener noreferrer">Comprar</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      )}
 
       {/* FOOTER */}
       <Footer />
